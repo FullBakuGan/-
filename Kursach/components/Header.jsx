dc29e3a0a410a4f363da2e4fc56aedfb81ce1664
FullBakuGan <nikosmelik666@gmail.com>
2025-09-06 21:21:35 +0300

    Создание рабочей среды

---
+++ Kursach/components/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css'
+export default function Header(){
+    return(
+        <header>
+            <h1>Онлайн запись в лучшую парикмахерскую города</h1>
+        </header>
+    )
+}
---
+++ Kursach/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from '../components/Header'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <main>
+          Наше приложение
+        </main>
+      </div>
+    </>
+  )
+}
